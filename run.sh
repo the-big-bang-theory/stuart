f67a1e434fca058c203deb7717b3da7392596921
k33g <ph.charriere@gmail.com>
2017-12-08 06:56:26 +0100

    gardening

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ SERVICE_NAME=stuart \
 SERVICE_ROOT=hey \
 PORT=8080 \
 SERVICE_PORT=8080 \
-REDIS_URL=redis://127.0.0.1:6379 \
+REDIS_HOST=127.0.0.1 \
+REDIS_PORT=6379 \
 java  -jar target/stuart-1.0-SNAPSHOT-fat.jar
